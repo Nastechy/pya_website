2f44e8a959e5cdf5773121edb63b108c653c1428
Nastechy <Ibinichinasajoy@gmail.com>
2025-08-10 16:10:02 +0100

    modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,36 @@ import StatsSection from "@/components/stats-section";
 import SupportSection from "@/components/support-section";
 
 
+
+export const metadata = {
+  title: "Pan Youth Association (PYA)",
+  description: "A dynamic Association dedicated to uplifting and empowering young people from diverse backgrounds",
+  alternates: {
+    canonical: "https://www.pyaabuja.com/",
+  },
+  openGraph: {
+    title: "Pan Youth Association (PYA)",
+    description: "A dynamic Association dedicated to uplifting and empowering young people from diverse backgrounds",
+    url: "https://www.pyaabuja.com/",
+    images: [
+      {
+        url: "/Newpanlogo.jpeg",
+        width: 800,
+        height: 600,
+        alt: "Pan Abuja Logo",
+      },
+    ],
+    siteName: "Pan Youth Association",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pan Youth Association (PYA)",
+    description: "A dynamic Association dedicated to uplifting and empowering young people from diverse backgrounds",
+    images: ["/Newpanlogo.jpeg"],
+  },
+};
+
+
 export default function Home() {
   return (
     <div className="min-h-screen relative">
